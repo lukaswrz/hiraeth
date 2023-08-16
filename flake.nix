75b6682cac2e34e3264d07cd25b94bb0f4792c6f
Lukas Wurzinger <lukas@wrz.one>
2023-08-16 21:35:23 +0200

    add Nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "hiraeth is a simple web-based file sharing program";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 20;
+      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gotools
+            gopls
+          ];
+        };
+      });
+    };
+}
